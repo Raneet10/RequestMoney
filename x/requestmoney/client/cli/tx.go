fc11ab43a0c931b4354a70ae26a8c0693ee7922f
Developer Experience team at Tendermint <hello@tendermint.com>
2021-01-26 12:23:07 +0530

    Initialized with Starport

---
+++ x/requestmoney/client/cli/tx.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/raneet10/requestmoney/x/requestmoney/types"
+)
+
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	requestmoneyTxCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	requestmoneyTxCmd.AddCommand(flags.PostCommands(
+    // this line is used by starport scaffolding # 1
+	)...)
+
+	return requestmoneyTxCmd
+}
