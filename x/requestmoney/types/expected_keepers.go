6674cc886a6f9ea91b9fc8e6f3df9021c67c2851
raneet <raneetdebnath10@gmail.com>
2021-02-01 11:20:49 +0530

    update expected keepers

--- x/requestmoney/types/expected_keepers.go
+++ x/requestmoney/types/expected_keepers.go
@@ -16,10 +16,11 @@ import (
 When a module wishes to interact with another module, it is good practice to define what it will use
 as an interface so the module cannot use things that are not permitted.
 TODO: Create interfaces of what you expect the other keepers to have to be able to use this module.*/
-type MintKeeper interface {
+/*type MintKeeper interface {
 	//SubtractCoins(ctx sdk.Context, addr sdk.AccAddress, amt sdk.Coins) (sdk.Coins, error)
 	MintCoins(ctx sdk.Context, newCoins sdk.Coins) error
-}
+}*/
 type SupplyKeeper interface {
+	MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 }
